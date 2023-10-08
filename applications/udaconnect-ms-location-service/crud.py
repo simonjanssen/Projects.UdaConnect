481038e3422c95d40f2a0bf6ef598de7dbd7eaac
Simon Janssen <janssen.simon@posteo.de>
2023-10-08 11:36:54 +0000

    replace deprecated bulk insert statement

--- applications/udaconnect-ms-location-service/crud.py
+++ applications/udaconnect-ms-location-service/crud.py
@@ -1,3 +1,4 @@
+from sqlalchemy import insert
 from sqlalchemy.orm import Session
 from typing import List
 
@@ -10,6 +11,6 @@ def create_location(db: Session, location: dict) -> None:
     return
 
 def create_locations(db: Session, locations: List[dict]) -> None:
-    db.bulk_insert_mappings(models.Location, locations)
+    db.execute(insert(models.Location), locations)
     db.commit()
     return
